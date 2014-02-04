b5d05393537105117045b627741d75fdecdb632f
Yiheng <csulaq@warwick.ac.uk>
2014-02-04 21:42:00 +0000

    Register page done

--- register.php
+++ register.php
@@ -20,108 +20,135 @@
 		<?php
 			//attempt connection to MySQL
 			$mysqli = new mysqli("localhost", "root","","Tastify");
-				if ($mysqli == false) {
-					die("Error: Could not connect. " . mysql_connect_error());
-				} 
+			$dbc = mysqli_connect("localhost", "root","","Tastify");    
+			//Check if the connection is successful
+			if ($mysqli == false) {
+				die("Error: Could not connect. " . mysql_connect_error());
+			} 
 
 			if (isset($_POST['submit'])) {
-
-
 				// retrive and check input values
 				$inputError = false;
-				if (empty($_POST['email'])) {
-					echo "Enter valid username";
+				if ($inputError != true && empty($_POST['email'])) {
+					echo "Enter valid username <br>";
 					$inputError = true;
 				} else {
 					$email = $mysqli->escape_string($_POST['email']);
 				}
 				if ($inputError != true && empty($_POST['password'])) {
-					echo "Enter valid password plz";
+					echo "Enter valid password plz <br>";
 					$inputError = true;
 				} else {
 					$password = $mysqli->escape_string($_POST['password']);
 				}
+				if ($inputError != true && empty($_POST['password2'])) {
+					echo "Confirm Your password plz <br>";
+					$inputError = true;
+				} else {
+					$password2 = $mysqli->escape_string($_POST['password2']);
+					if ($password != $password2) {
+						echo "Your confirmation password dose not match with the 1st one, Please check <br>";
+						$inputError = true;
+					} else {
+						sha1('$password');
+					}
+				}
 				if ($inputError != true && empty($_POST['gender'])) {
-					echo "Enter valid gender plz";
+					echo "Enter valid gender plz <br>";
 					$inputError = true;
 				} else {
 					$gender = $mysqli->escape_string($_POST['gender']);
 				}
 				if ($inputError != true && empty($_POST['fname'])) {
-					echo "Enter valid first name plz";
+					echo "Enter valid first name plz <br>";
 					$inputError = true;
 				} else {
 					$fname = $mysqli->escape_string($_POST['fname']);
 				}
 				if ($inputError != true && empty($_POST['lname'])) {
-					echo "Enter valid last name plz";
+					echo "Enter valid last name plz <br>";
 					$inputError = true;
 				} else {
 					$lname = $mysqli->escape_string($_POST['lname']);
 				}
 
 				$option_vegi = $mysqli->escape_string($_POST['vegi']);
+				if ($option_vegi != 1) {
+					$option_vegi = 0;
+				}
 				$option_light = $mysqli->escape_string($_POST['diet']);
+				if ($option_light != 1) {
+					$option_light = 0;
+				}
 				$option_hot = $mysqli->escape_string($_POST['spice']);
+				if ($option_hot != 1) {
+					$option_hot = 0;
+				}
 
 				if ($inputError == false) {
 					$sql = "INSERT INTO user_db (email,password,gender,fname,lname,option_vegi,option_light,option_hot) 
 						VALUES ('$email','$password','$gender','$fname','$lname','$option_vegi','$option_light','$option_hot');";
-						
-					if ($mysqli->query($sql) == true) {
-						echo "New user added with ID: " . $mysqli->insert_id;}
+
+					$checkUsername = "select * from user_db where email = '$email'";
+					$checkResult = mysqli_query($dbc, $checkUsername);
+
+
+					if (mysqli_num_rows($checkResult) == 0) {
+						if ($mysqli->query($sql) == true) {
+								echo "New user added with ID: " . $mysqli->insert_id;
+								
+							} else {
+								echo  $mysqli->error;
+							}
 					} else {
-						echo "ERROR: $sql. " . $mysqli->error;
+						echo "Username has already been taken, try another one";
+						$email = "";
 					}
 				}
-					
-				//close message block
-				echo "</div>";		
-			
+			}
 		?>
 
-
-			<form method="POST" action=""register.php"" id="registerForm">
-			  <div data-role="fieldcontain">
-			    <label for="email">User Name:</label>
-			    <input type="text" name="email" id="email"   placeholder="At least 5 letters long">
-			  </div>
-			  <div data-role="fieldcontain">
+		<form method="POST" action=""register.php"" id="registerForm">
+			<div data-role="fieldcontain">
+				<label for="email">User Name:</label>
+			    <input type="text" name="email" id="email"   placeholder="At least 5 characters long" value="<?php if (!empty($email)) echo $email; ?>">
+			</div>
+			<div data-role="fieldcontain">
 			    <label for="password">Password:</label>
-			    <input type="text" name="password" id="password"   placeholder="At least 5 letters long">
-			  </div>
-			  <div data-role="fieldcontain">
+			    <input type="password" name="password" id="password"   placeholder="At least 5 letters long"  value="<?php if (!empty($password)) echo $password; ?>">
+			</div>
+			<div data-role="fieldcontain">
 			    <label for="password2">Confirm Password:</label>
-			    <input type="text" name="password2" id="password2"   placeholder="Confirm Your Password">
-			  </div>
-			  <div data-role="fieldcontain">
+			    <input type="password" name="password2" id="password2"   placeholder="Confirm Your Password"  value="<?php if (!empty($password2)) echo $password2; ?>">
+			</div>
+			<div data-role="fieldcontain">
 			    <label for="fname">First Name: </label>
-			    <input type="text" name="fname" id="fname"   placeholder="Your First Name..">
-			  </div>
-			  <div data-role="fieldcontain">
+			    <input type="text" name="fname" id="fname"   value="<?php if (!empty($fname)) echo $fname; ?>">
+			</div>
+			<div data-role="fieldcontain">
 			    <label for="lname">Last name:</label>
-			    <input type="text" name="lname" id="lname"   placeholder="Your Last Name..">
-			  </div>
-			  <div data-role="fieldcontain">
+			    <input type="text" name="lname" id="lname"   value="<?php if (!empty($lname)) echo $lname; ?>">
+			</div>
+			<div data-role="fieldcontain">
 			    <label for="gender">Your Gender:</label>
 			    <select name="gender" id="gender" data-role="slider" >
-			      <option value="Male">Male</option>
-			      <option value="Female">Female</option>
+			    	<option value="Male">Male</option>
+			    	<option value="Female">Female</option>
 			    </select>
-			  </div>
-			  <div data-role="fieldcontain">
+			</div>
+			<div data-role="fieldcontain">
 		        <fieldset data-role="controlgroup">
-		          <legend>Please Tick If You Are </legend>
-		            <label for="vegi">A vegi</label>
-		            <input type="checkbox" name="vegi" id="vegi" value="1">
-		            <label for="diet">On Diet</label>
-		            <input type="checkbox" name="diet" id="diet" value="1">
-		            <label for="spice">Spicy Food Lover</label>
-		            <input type="checkbox" name="spice" id="spice" value="1">	
+		        	<legend>Please Tick If You Are </legend>
+		            	<label for="vegi">A vegi</label>
+		            	<input type="checkbox" name="vegi" id="vegi" value="1">
+		            	<label for="diet">On Diet</label>
+		            	<input type="checkbox" name="diet" id="diet" value="1">
+		            	<label for="spice">Spicy Food Lover</label>
+		            	<input type="checkbox" name="spice" id="spice" value="1">	
 		        </fieldset>
-		      </div>
-		      <input type="submit" name="submit" value="Register">
-			</form>		
+		    </div>
+		    <input type="submit" name="submit" value="Register">
+		</form>		
 		</div>
 	</div>
 </body>
